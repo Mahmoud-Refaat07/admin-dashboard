85cdb8e650379fbfaf99add196e7af80f36b6873
MahmoudRefaat07 <mahmoud.refaat.career@gmail.com>
2026-03-26 14:04:38 +0200

    implemented calendar page

--- src/pages/CalenderPage.tsx
+++ src/pages/CalenderPage.tsx
@@ -1,5 +1,37 @@
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import interactionPlugin from "@fullcalendar/interaction";
+import { useState } from "react";
+
 const CalenderPage = () => {
-  return <div>CalenderPage</div>;
+  const [events, setEvents] = useState([
+    { title: "event 1", date: "2026-04-1" },
+    { title: "event 2", date: "2026-04-2" },
+  ]);
+  const handleDateClick = (arg: any) => {
+    const title = prompt("What event do you want to add?");
+    if (title) {
+      setEvents([...events, { title: title, date: arg.dateStr }]);
+    }
+  };
+
+  return (
+    <FullCalendar
+      plugins={[dayGridPlugin, interactionPlugin]}
+      eventContent={renderEventContent}
+      events={events}
+      dateClick={handleDateClick}
+    />
+  );
 };
 
 export default CalenderPage;
+
+function renderEventContent(eventInfo: any) {
+  return (
+    <>
+      <b>{eventInfo.timeText}</b>
+      <i>{eventInfo.event.title}</i>
+    </>
+  );
+}
