e1fe5a36ac97addfd5273d2cc7d5a70af437c359
MahmoudRefaat07 <mahmoud.refaat.career@gmail.com>
2026-03-27 15:02:46 +0200

    implemented pie chart page

--- src/pages/PieChartPage.tsx
+++ src/pages/PieChartPage.tsx
@@ -1,5 +1,26 @@
+import { PieChart } from "@mui/x-charts/PieChart";
+import UpperTitles from "../components/UpperTitles";
+
 const PieChartPage = () => {
-  return <div>PieChartPage</div>;
+  return (
+    <>
+      <UpperTitles title="Pie Chart" subTitle="Simple pie charts" />
+      <PieChart
+        sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}
+        series={[
+          {
+            data: [
+              { id: 0, value: 80, label: "React" },
+              { id: 1, value: 60, label: "Vue" },
+              { id: 2, value: 70, label: "Angular" },
+            ],
+          },
+        ]}
+        width={500}
+        height={500}
+      />
+    </>
+  );
 };
 
 export default PieChartPage;
