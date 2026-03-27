8428af4b7261fbd74c3b242e7570334d0b93a338
MahmoudRefaat07 <mahmoud.refaat.career@gmail.com>
2026-03-27 15:07:41 +0200

    implemented line chart page

--- src/pages/LineChartPage.tsx
+++ src/pages/LineChartPage.tsx
@@ -1,5 +1,31 @@
+import { LineChart } from "@mui/x-charts/LineChart";
+import UpperTitles from "../components/UpperTitles";
+
 const LineChartPage = () => {
-  return <div>LineChartPage</div>;
+  return (
+    <>
+      <UpperTitles title="Line Chart" subTitle="Simple line Chart" />
+      <LineChart
+        xAxis={[{ data: [1, 2, 3, 5, 8, 10, 12, 15, 16] }]}
+        series={[
+          {
+            data: [2, 5.5, 2, 8.5, 1.5, 5],
+            valueFormatter: (value) =>
+              value == null ? "NaN" : value.toString(),
+          },
+          {
+            data: [null, null, null, null, 5.5, 2, 8.5, 1.5, 5],
+          },
+          {
+            data: [7, 8, 5, 4, null, null, 2, 5.5, 1],
+            valueFormatter: (value) => (value == null ? "?" : value.toString()),
+          },
+        ]}
+        height={500}
+        margin={{ bottom: 10 }}
+      />
+    </>
+  );
 };
 
 export default LineChartPage;
