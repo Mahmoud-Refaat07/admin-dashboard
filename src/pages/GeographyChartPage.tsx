a65590edf9d18c2a07b8019c5c8308b1d021ecff
MahmoudRefaat07 <mahmoud.refaat.career@gmail.com>
2026-03-28 14:01:59 +0200

    implemented geography chart page

--- src/pages/GeographyChartPage.tsx
+++ src/pages/GeographyChartPage.tsx
@@ -1,5 +1,26 @@
+import { RadarChart } from "@mui/x-charts/RadarChart";
+import UpperTitles from "../components/UpperTitles";
+
 const GeographyChartPage = () => {
-  return <div>GeographyChartPage</div>;
+  return (
+    <>
+      <UpperTitles title="Geography Chart" subTitle="Study subjects groups" />
+      <RadarChart
+        height={500}
+        series={[{ label: "Lisa", data: [120, 98, 86, 99, 85, 65] }]}
+        radar={{
+          metrics: [
+            { name: "Math", max: 120 },
+            { name: "Chinese", max: 120 },
+            { name: "English", max: 120 },
+            { name: "Geography", max: 120 },
+            { name: "Physics", max: 120 },
+            { name: "History", max: 120 },
+          ],
+        }}
+      />
+    </>
+  );
 };
 
 export default GeographyChartPage;
